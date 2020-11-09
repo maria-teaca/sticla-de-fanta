06da1e5829b6075cdd47f9814b1800cc238f6596
Maria Teaca <maria.teaca@stud.acs.pub.ro>
2020-11-09 07:26:49 +0200

    Refacem repo ul

---
+++ README.md
@@ -0,0 +1,3 @@
+Sticla de fanta este frumoasa si are buline. Aici vom freea multe sticle de fanta diferite.
+
+Am scris crea gresit. Greseala mea.
---
+++ fanta.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("o  o  .. o.\n");
+	printf(".. o . o ..\n");
+	printf("o. o. o. o.\n");
+
+	return 0;
+}
+
